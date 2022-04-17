b9583bbe04cd2d9d20a9b2cc6d340ff9a93cc52f
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2022-04-17 15:41:12 -0400

    Removed unused pearl file. Added Python Sql script
    New SQL database accepts connection via Python / PostgreSQL

---
+++ Script_PostgreSQL.py
@@ -0,0 +1,39 @@
+import os
+import psycopg2
+
+
+def exec_statement(conn, stmt):
+    try:
+        with conn.cursor() as cur:
+            cur.execute(stmt)
+            row = cur.fetchone()
+            conn.commit()
+            if row: print(row[0])
+    except psycopg2.ProgrammingError:
+        return
+
+
+def main():
+
+    # Connect to CockroachDB
+    #connection = psycopg2.connect(os.environ['DATABASE_URL'])
+    connection = psycopg2.connect(host="free-tier11.gcp-us-east1.cockroachlabs.cloud", port=26257, dbname="terra-hamster-556.defaultdb", user="dev_link", password="7sHSHqWHzdQJUrFrFclaSg")
+
+    statements = [
+        # CREATE the messages table
+        "CREATE TABLE messages (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), message STRING)",
+        # INSERT a row into the messages table
+        "INSERT INTO messages (message) VALUES ('Hello world!')",
+        # SELECT a row from the messages table
+        "SELECT message FROM messages"
+    ]
+
+    for statement in statements:
+        exec_statement(connection, statement)
+
+    # Close communication with the database
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
